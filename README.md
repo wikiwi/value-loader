5a14fc89223a50431b8e8942aa8690249bebf24e
Chi Vinh Le <vinh@wikiwi.io>
2017-02-04 22:14:58 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Value Loader for Webpack
+
+Like [val-loader](https://github.com/webpack-contrib/val-loader) but runs
+file in a child compiler.
+
+## Usage
+
+``` javascript
+let a = require("value-loader!./file.js");
+// => excute file.js while compiling and
+//    take the result as javascript code for including
+```
+
+This loader is also useful if you want to provide data for another loader:
+
+``` javascript
+require("css-loader!val-loader!./generateCss.js");
+```
+
+## Config
+
+Instead of getting the whole `exports` object you can set `name` to the
+named export you want to retrieve.
+
+## Acknowledgement
+
+This work is heavily based on the work done by Nathan Tran for
+[css-in-js-loader](https://github.com/nthtran/css-in-js-loader).
+
+## License
+
+MIT
---
+++ value-loader.js
@@ -0,0 +1,110 @@
+const path = require("path");
+const fs = require("fs");
+const NodeTemplatePlugin = require("webpack/lib/node/NodeTemplatePlugin");
+const NodeTargetPlugin = require("webpack/lib/node/NodeTargetPlugin");
+const LibraryTemplatePlugin = require("webpack/lib/LibraryTemplatePlugin");
+const SingleEntryPlugin = require("webpack/lib/SingleEntryPlugin");
+const LimitChunkCountPlugin = require("webpack/lib/optimize/LimitChunkCountPlugin");
+const loaderUtils = require("loader-utils");
+
+module.exports = function (source) {
+  if (this.cacheable) this.cacheable();
+  return source;
+};
+
+module.exports.pitch = function (request, prevRequest) {
+  if (this.cacheable) this.cacheable();
+  const callback = this.async();
+  if ([".js", ".ts"].indexOf(path.extname(request)) >= 0) {
+    produce(this, request, callback, loaderUtils.getLoaderConfig(this));
+  } else {
+    const parts = request.split("!");
+    const filename = parts[parts.length - 1];
+    this.addDependency(filename);
+    fs.readFile(filename, "utf8", callback);
+  }
+};
+
+function produce(loader, request, callback, config) {
+  const childFilename = "css-in-js-output-filename";
+  const outputOptions = { filename: childFilename };
+  const childCompiler = getRootCompilation(loader)
+      .createChildCompiler("css-in-js-compiler", outputOptions);
+  childCompiler.apply(new NodeTemplatePlugin(outputOptions));
+  childCompiler.apply(new LibraryTemplatePlugin(null, "commonjs2"));
+  childCompiler.apply(new NodeTargetPlugin());
+  childCompiler.apply(new SingleEntryPlugin(loader.context, `!!${request}`));
+  childCompiler.apply(new LimitChunkCountPlugin({ maxChunks: 1 }));
+  const subCache = `subcache ${__dirname} ${request}`;
+  childCompiler.plugin("compilation", (compilation) => {
+    if (compilation.cache) {
+      if (!compilation.cache[subCache])
+              { compilation.cache[subCache] = {}; }
+      compilation.cache = compilation.cache[subCache];
+    }
+  });
+  // We set loaderContext[__dirname] = false to indicate we already in
+  // a child compiler so we don't spawn another child compilers from there.
+  childCompiler.plugin("this-compilation", (compilation) => {
+    compilation.plugin("normal-module-loader", (loaderContext) => {
+      loaderContext[__dirname] = false;
+    });
+  });
+  let source;
+  childCompiler.plugin("after-compile", (compilation, callback) => {
+    source = compilation.assets[childFilename] && compilation.assets[childFilename].source();
+
+    // Remove all chunk assets
+    compilation.chunks.forEach((chunk) => {
+      chunk.files.forEach((file) => {
+        delete compilation.assets[file];
+      });
+    });
+
+    callback();
+  });
+
+  childCompiler.runAsChild((err, entries, compilation) => {
+    if (err) return callback(err);
+
+    if (compilation.errors.length > 0) {
+      return callback(compilation.errors[0]);
+    }
+    if (!source) {
+      return callback(new Error("Didn't get a result from child compiler"));
+    }
+    compilation.fileDependencies.forEach((dep) => {
+      loader.addDependency(dep);
+    }, loader);
+    compilation.contextDependencies.forEach((dep) => {
+      loader.addContextDependency(dep);
+    }, loader);
+
+    let exports;
+    try {
+      exports = loader.exec(source, request);
+    } catch (e) {
+      return callback(e);
+    }
+    if (exports) {
+      const name = config.name;
+      if (name && name in exports) {
+        exports = exports[name];
+      }
+      console.log(exports);
+      callback(null, exports);
+    } else {
+      callback();
+    }
+  });
+}
+
+function getRootCompilation(loader) {
+  let compiler = loader._compiler;
+  let compilation = loader._compilation;
+  while (compiler.parentCompilation) {
+    compilation = compiler.parentCompilation;
+    compiler = compilation.compiler;
+  }
+  return compilation;
+}
